0433dad27f5ed322b86ed5b460f86d4b0ef9ff53
Lokeshwaran Basha <lokeshwaranbasha@AGLS-MLT-460.local>
2022-04-22 08:32:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import Sidebar from './components/Sidebar'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import {Applications, AllApplications, AddNewApp} from './pages/Applications'
+import {Shopify, AllShopify, AddNewShop} from './pages/Shopify'
+import {AuthenticationProviders, AllAuthenticationProviders, AddNewAuth} from './pages/AuthenticationProviders'
+import Dashboard from './pages/Dashboard'
+import {Providers, AllProviders, AddNewPro} from './pages/Providers'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Sidebar/>
+    <Routes>
+      <Route exact path="/dashboard" element={<Dashboard/>}/>
+      <Route path='/applications' element={<Applications/>}/>
+      <Route path='/applications/allapplications' element={<AllApplications/>}/>
+      <Route path='/applications/addnew' element={<AddNewApp/>}/>
+      <Route path='/shopifystores' element={<Shopify/>}/>
+      <Route path='/shopifystores/allshopifystores' element={<AllShopify/>}/>
+      <Route path='/shopifystores/addnew' element={<AddNewShop/>}/>
+      <Route path='/authenticationproviders' element={<AuthenticationProviders/>}/>
+      <Route path='/authenticationproviders/allauthenticationproviders' element={<AllAuthenticationProviders/>}/>
+      <Route path='/authenticationproviders/addnew' element={<AddNewAuth/>}/>
+      <Route path='/providers' element={<Providers/>}/>
+      <Route path='/providers/allproviders' element={<AllProviders/>}/>
+      <Route path='/providers/addnew' element={<AddNewPro/>}/>
+
+    </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Navbar,Container} from 'react-bootstrap'
+function Dashboard() {
+  return (
+    <div className="dashboard">
+        <h1>Dashboard</h1>
+        {/* <Container>
+  <Navbar expand="lg" variant="light" bg="light">
+    <Container>
+      <Navbar.Brand href="#">Navbar</Navbar.Brand>
+    </Container>
+  </Navbar>
+</Container> */}
+    </div>
+  )
+}
+
+export default Dashboard
